524eb7bc68166c08f5a3a566b94dcfdb2824410c
Tawicha Sirisuwan <6510110168@psu.ac.th>
2023-02-27 10:05:12 +0700

    Create Activity-Card .tsx

---
+++ client/src/components/activity-card.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+function ActivityCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://ichef.bbci.co.uk/news/640/cpsprodpb/1496F/production/_116553348_mansleepsgettyimages.png"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Activity name
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            เนื้อหากิจกรรม
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Readmore
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+export default  ActivityCard;
