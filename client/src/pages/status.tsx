2c27bf3451d1f64cf16eec4e0cfc409159b06e57
Tawicha Sirisuwan <6510110168@psu.ac.th>
2023-02-28 00:12:49 +0700

    Create Status Page

---
+++ client/src/pages/status.tsx
@@ -0,0 +1,33 @@
+import "./pages/statuspage.css";
+import ActivityAppBar from "../components/app-bar"
+import StatusCard from "../components/statuscard";
+
+
+
+function Status() {
+  return(
+   <html>
+    <ActivityAppBar></ActivityAppBar>
+    <link
+          href="https://fonts.googleapis.com/css2?family=Kanit&display=swap"
+          rel="stylesheet"
+        />
+    <body>
+      <div className="line">
+        <p className="text1">สถานะการเข้าร่วมกิจกรรม</p>
+      </div>
+      <div className="container-grid">
+        <StatusCard></StatusCard>
+        <StatusCard></StatusCard>
+        <StatusCard></StatusCard>
+        <StatusCard></StatusCard>
+      </div>
+      <div>
+        
+      </div>
+    </body>
+   </html>
+  )
+}
+
+export default Status;
