dde36a9380d6322cf858b5dce0fca0ad88e8711e
Siwakorn Noylam <6510110465@gmail.psu.ac.th>
2023-02-28 16:06:38 +0700

    create history.tsx

---
+++ client/src/pages/history.tsx
@@ -0,0 +1,23 @@
+import "./pages/history.css";
+import ActivityAppBar from "../components/app-bar"
+function Status() {
+  return(
+   <html>
+    <ActivityAppBar></ActivityAppBar>
+    <link
+          href="https://fonts.googleapis.com/css2?family=Kanit&display=swap"
+          rel="stylesheet"
+        />
+    <body>
+    <div className="box">
+            <p>
+                <label>ลงทะเบียนเข้าร่วมกิจกรรม</label>
+            </p>  
+      </div>
+
+    </body>
+   </html>
+  )
+}
+
+export default Status;
