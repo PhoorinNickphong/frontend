f3369b9f87baebb192df51763c0fe20f15d66205
Tawan Sukapat <6510110158@email.psu.ac.th>
2023-03-02 01:26:34 +0700

    Add contact and create contactcard

---
+++ client/src/components/contactcard/contact-card2.tsx
@@ -0,0 +1,30 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import '../contactcard/contactcard.css';
+
+function ContactCard2() {
+    return (
+      <Card sx={{ maxWidth: 400 }}>
+          <CardMedia
+            component="img"
+            height="50%"
+            image="https://s.isanook.com/ca/0/ui/279/1396205/download20190701165129_1562561119.jpg?ip/convert/w0/q80/webp"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              6510110
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              นาย
+            </Typography>
+          </CardContent>
+      </Card>
+      
+    );
+  }
+  
+  
+  export default  ContactCard2;
